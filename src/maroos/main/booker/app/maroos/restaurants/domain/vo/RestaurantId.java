91c618fa7d81a6e4fd7fdef07442bb5dd2f79703
David Esneider Herrera Isaza <david.herrera@sofka.com.co>
2021-07-21 18:36:36 -0500

    Validacion UUID y Test unitario
    Se valida que VO RestaurantID que el id sea un UUID valido y se realiza su respectivo test unitario en caso de que halla un UUID valido

--- src/maroos/main/booker/app/maroos/restaurants/domain/vo/RestaurantId.java
+++ src/maroos/main/booker/app/maroos/restaurants/domain/vo/RestaurantId.java
@@ -2,13 +2,20 @@ package booker.app.maroos.restaurants.domain.vo;
 
 import booker.app.shared.domain.Identifier;
 
+import java.util.UUID;
+
 public class RestaurantId extends Identifier {
 
     public RestaurantId(String value) {
         super(value);
+        ensureValidUuid(value);
     }
 
     private RestaurantId() {
         super("");
+    }
+
+    private void ensureValidUuid(String value) throws IllegalArgumentException {
+        UUID.fromString(value);
     }
 }
--- src/maroos/test/booker/app/maroos/restaurants/application/create/RestaurantCreatorUseCaseTest.java
+++ src/maroos/test/booker/app/maroos/restaurants/application/create/RestaurantCreatorUseCaseTest.java
@@ -5,23 +5,22 @@ import booker.app.maroos.restaurants.domain.RestaurantRepository;
 import booker.app.maroos.restaurants.domain.vo.*;
 import booker.app.shared.domain.RestaurantCreatedDomainEvent;
 import booker.app.shared.domain.bus.event.EventBus;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
 import java.util.Collections;
-
 import static org.mockito.Mockito.*;
 
 class RestaurantCreatorUseCaseTest {
 
 
     @Test
-    void save_a_restaurant() throws Exception{
+    void save_a_valid_restaurant() throws Exception{
         RestaurantRepository repository = mock(RestaurantRepository.class);
         EventBus eventBus = mock(EventBus.class);
         RestaurantCreatorUseCase useCase = new RestaurantCreatorUseCase(repository, eventBus);
 
         CreateRestaurantRequest request = new CreateRestaurantRequest(
-                "123456789",
+                "1aab45ba-3c7a-4344-8936-78466eca77fa",
                 "El Mejor",
                 "lo mejor de lo mejor",
                 "Mediterranea",
@@ -30,7 +29,6 @@ class RestaurantCreatorUseCaseTest {
                 "http://sitioweb.com.co",
                 "3046518"
         );
-
 
         Restaurant restaurant = new Restaurant(
                 new RestaurantId(request.restaurantId()),
@@ -57,6 +55,42 @@ class RestaurantCreatorUseCaseTest {
 
         verify(repository, atLeastOnce()).save(restaurant);
         verify(eventBus, atLeastOnce()).publish(Collections.singletonList(domainEvent));
+    }
+
+    @Test
+    void save_a_invalid_restaurantId() throws IllegalArgumentException {
+        RestaurantRepository repository = mock(RestaurantRepository.class);
+        EventBus eventBus = mock(EventBus.class);
+        RestaurantCreatorUseCase useCase = new RestaurantCreatorUseCase(repository, eventBus);
+
+        CreateRestaurantRequest request = new CreateRestaurantRequest(
+                "123456789",
+                "El Mejor",
+                "lo mejor de lo mejor",
+                "Mediterranea",
+                "lo que hay es lo mejor",
+                "http://logo.jpg",
+                "http://sitioweb.com.co",
+                "3046518"
+        );
+
+        try{
+            Restaurant restaurant = new Restaurant(
+                    new RestaurantId(request.restaurantId()),
+                    new RestaurantName(request.restaurantName()),
+                    new RestaurantAbstract(request.restaurantAbstarct()),
+                    new RestaurantSpecialities(request.restaurantSpecialities()),
+                    new RestaurantSlogan(request.restaurantSlogan()),
+                    new RestaurantLogo(request.restaurantLogo()),
+                    new RestaurantWebSite(request.restaurantWebSite()),
+                    new RestaurantPhone(request.restaurantPhone())
+            );
+
+            throw new NullPointerException("The UUID is correct !!");
+        }catch (IllegalArgumentException error){
+            Assert.assertEquals(error.getMessage(), "Invalid UUID string: 123456789");
+            Assert.assertTrue(error instanceof IllegalArgumentException);
+        }
     }
 
 }
